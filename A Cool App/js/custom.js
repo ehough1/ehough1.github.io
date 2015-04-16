b4cc5ed49ef90487f97b653b6de5c9598a383385
ehough1 <k00176880@student.lit.ie>
2015-04-16 15:47:06 +0000

    Added all names functionality

--- A Cool App/js/custom.js
+++ A Cool App/js/custom.js
@@ -1,7 +1,38 @@
 $(document).ready(function(){
   
   $('button#yourNameButton').click(function(){
-    console.log('yourName Clicked');
+    $.ajax({
+      method: "GET",
+      url: "//lit-basin-4940.herokuapp.com/api/yourName"
+    }).done(function(response){
+      console.log(response);
+      $('div#yourName').html("<p>"+response.name+"</p>");
+    }).fail(function(msg){
+      
+    });
+  });
+  
+  $('button#myName').click(function(){
+    console.log("button clicked");
+    
+    var typedName = $('input#myName').val();
+    
+    $.ajax({
+      method: "POST",
+      data: {myName: typedName},
+      url: "//lit-basin-4940.herokuapp.com/api/myName"
+    }).done(function(response){
+      console.log(response.allNames);
+      var theNamesHtml = '<ul id="allNames">';
+      for(var i=0; i < response.allNames.length; i++){
+        theNamesHtml += "<li>" +response.allNames[i] + "</li>";
+      }
+      
+      theNamesHtml += "</ul>"
+      $('div#myNameResponse').html(theNamesHtml);
+    }).fail(function(msg){
+      console.log(msg);
+    })
   });
   
 });
